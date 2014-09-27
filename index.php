13696bb8f8c242d1c269923b0130e4e4c9ce906f
Gabriel Paladino <l31rb4g@gmail.com>
2014-09-27 20:52:24 -0300

    gogo

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Gabriel Paladino</title>
+
+    <link rel="stylesheet" href="css/reset.css"/>
+    <link rel="stylesheet" href="css/main.css?t=<?php echo time(); ?>"/>
+
+    <script type="text/javascript" src="js/mootools-core-1.5.0-full-nocompat-yc.js"></script>
+    <script type="text/javascript" src="js/interface.js?t=<?php echo time(); ?>"></script>
+    <script type="text/javascript" src="js/flying_char.js?t=<?php echo time(); ?>"></script>
+    <script type="text/javascript" src="js/flying_object.js?t=<?php echo time(); ?>"></script>
+    <script type="text/javascript" src="js/flying_string.js?t=<?php echo time(); ?>"></script>
+    <script type="text/javascript" src="js/main.js?t=<?php echo time(); ?>"></script>
+
+</head>
+<body>
+
+</body>
+</html>
