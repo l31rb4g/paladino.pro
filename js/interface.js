5591ebe50cdd56d3b4b5b24f3ee2fea29ff8aa11
Gabriel Paladino <l31rb4g@gmail.com>
2014-09-27 21:16:53 -0300

    gogo

--- js/interface.js
+++ js/interface.js
@@ -35,8 +35,7 @@ Interface = {
 
         frame.addElement('+', margin, margin);
 
-        var qtd_y = Math.ceil((window.getSize().y - (margin * 2)) / 32) - 1;
-        console.log('wtd', qtd_y)
+        var qtd_y = Math.ceil((window.getSize().y - (margin * 2)) / 33);
         var qtd_x = ((window.getSize().x - (margin * 2)) / 17).toInt() - 3;
 
         //left 1
