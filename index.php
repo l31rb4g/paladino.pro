3a9bb357938332539ef72677e100669f15b8f0cc
Gabriel Paladino <l31rb4g@gmail.com>
2014-10-04 19:39:48 -0300

    html comment on header

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
     <link rel="stylesheet" href="css/reset.css"/>
     <link rel="stylesheet" href="css/main.css?t=<?php echo time(); ?>"/>
 
+    <!-- The only external JS library used in this game is mootools-core -->
     <script type="text/javascript" src="js/mootools-core-1.5.0-full-nocompat-yc.js"></script>
+
     <script type="text/javascript" src="js/interface.js?t=<?php echo time(); ?>"></script>
     <script type="text/javascript" src="js/flying_char.js?t=<?php echo time(); ?>"></script>
     <script type="text/javascript" src="js/flying_object.js?t=<?php echo time(); ?>"></script>
