6f4d0db42d3adfa2e17d2d3626ffbdf73c97b4c3
Gabriel Paladino <gabriel.paladino@pearson.com>
2015-10-30 15:09:44 -0200

    removendo comment

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <link rel="stylesheet" href="css/reset.css"/>
     <link rel="stylesheet" href="css/main.css?t=<?php echo time(); ?>"/>
 
-    <!-- The only external JS library used in this page is mootools-core -->
     <script type="text/javascript" src="js/mootools-core-1.5.0-full-nocompat-yc.js"></script>
 
     <script type="text/javascript" src="js/interface.js?t=<?php echo time(); ?>"></script>
