e49fe2f689d7543d1dfd4a6a2aad486efa38be60
Gabriel Paladino <gabriel.paladino@stormsec.com.br>
2017-12-19 15:37:20 -0200

    notes

--- notes.php
+++ notes.php
@@ -1,4 +1,13 @@
-<DOCTYPE html>
+<?php
+if (isset($_POST['notes'])){
+  $notes = trim($_POST['notes']);
+  f = fopen('notes.txt', 'w')
+  f.write($notes);
+  f.close();
+  die;
+}
+?>
+<!DOCTYPE html>
 <html>
 <head>
   <title>Notes</title>
@@ -6,7 +15,7 @@
     body {
       overflow: hidden;
     }
-    pre {
+    textarea {
       width: 100%;
       height: 100%;
       padding: 5px;
@@ -14,19 +23,19 @@
   </style>
 </head>
 <body>
-  <pre	contenteditable="true"></pre>
+  <textarea></textarea>
 
   <script type="text/javascript" src="js/mootools-core-1.5.0-full-nocompat-yc.js"></script>
   <script type="text/javascript">
   window.addEvent('domready', function(){
-	  $$('pre')[0].addEvent('keydown', function(){
+	  $$('textarea')[0].addEvent('keydown', function(){
       if (window.timer){
         clearTimeout(window.timer);
       }
       window.timer = setTimeout(() => {
 			  new Request({
 				  data: {
-					  notes: encodeURIComponent(this.get('text'))
+					  notes: encodeURIComponent(this.get('value'))
 				  }
 			  }).send();
       }, 250);
