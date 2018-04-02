2261c92b6207ed2913d3da31595f01795f15fe19
Gabriel Paladino <l31rb4g@gmail.com>
2018-04-01 22:31:08 -0300

    date

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-if ($_SERVER['HTTP_HOST'] == '54.208.162.187'){
-	die;
+if ($_SERVER['HTTP_HOST'] == '34.234.32.45'){
+    die;
 }
+$date = date('Ymd');
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,14 +11,14 @@ if ($_SERVER['HTTP_HOST'] == '54.208.162.187'){
     <title>Gabriel Paladino</title>
 
     <link rel="stylesheet" href="css/reset.css"/>
-    <link rel="stylesheet" href="css/main.css?t=<?php echo time(); ?>"/>
+    <link rel="stylesheet" href="css/main.css?t=<?php echo $date; ?>"/>
 
     <script type="text/javascript" src="js/mootools-core-1.5.0-full-nocompat-yc.js"></script>
-    <script type="text/javascript" src="js/interface.js?t=<?php echo 1; ?>"></script>
-    <script type="text/javascript" src="js/flying_char.js?t=<?php echo 1; ?>"></script>
-    <script type="text/javascript" src="js/flying_object.js?t=<?php echo 1; ?>"></script>
-    <script type="text/javascript" src="js/flying_string.js?t=<?php echo 1; ?>"></script>
-    <script type="text/javascript" src="js/main.js?t=<?php echo 1; ?>"></script>
+    <script type="text/javascript" src="js/interface.js?t=<?php echo $date; ?>"></script>
+    <script type="text/javascript" src="js/flying_char.js?t=<?php echo $date; ?>"></script>
+    <script type="text/javascript" src="js/flying_object.js?t=<?php echo $date; ?>"></script>
+    <script type="text/javascript" src="js/flying_string.js?t=<?php echo $date; ?>"></script>
+    <script type="text/javascript" src="js/main.js?t=<?php echo $date; ?>"></script>
 
 </head>
 <body>
