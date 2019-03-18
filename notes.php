642cf07acdad9b10fdf113861cf948168c8c3c52
Gabriel Paladino <l31rb4g@gmail.com>
2019-03-18 20:51:48 -0300

    auth

--- notes.php
+++ notes.php
@@ -6,7 +6,7 @@ if (!isset($_SERVER['PHP_AUTH_USER']) ||
         (!(
             $_SERVER['PHP_AUTH_USER'] == $user &&
             $_SERVER['PHP_AUTH_PW'] == $pass
-        )) {
+        ))) {
     header('WWW-Authenticate: Basic realm="Acesso restrito"');
     header('HTTP/1.0 401 Unauthorized');
     echo 'Acesso negado.';
